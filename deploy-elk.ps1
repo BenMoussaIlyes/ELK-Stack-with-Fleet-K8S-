d744040c9d43793abc99bd14e94fbdc703320674
Ilyes BEN MOUSSA <benmoussailyes13@gmail.com>
2024-08-25 23:40:43 +0200

    enhanched deploy script

--- deploy-elk.ps1
+++ deploy-elk.ps1
@@ -3,58 +3,79 @@ $labelSelector = "app=elasticsearch"
 $KibanaconfigMapFile = "kibana-config.yaml"
 $FleetconfigMapFile = "fleet-server-config.yaml"
 
+function Create-Token {
+    param (
+        [string]$tokenType  # Accepts either "kibana" or "fleet"
+    )
+    switch ($tokenType) {
+        "kibana" { $tokenOutput = kubectl exec -it $podName -n $namespace -- bin/elasticsearch-service-tokens create elastic/kibana kibana-token  }
+        "fleet" { $tokenOutput = kubectl exec -it $podName -n $namespace -- bin/elasticsearch-service-tokens create elastic/fleet-server fleet-server-token  }
+        default { throw "Invalid token type specified. Use 'kibana' or 'fleet'." }
+    }
+    Write-Host $tokenPath
+    return $tokenOutput
+}
+function Delete-Token {
+    param (
+        [string]$tokenType  # Accepts either "kibana" or "fleet"
+    )
+    switch ($tokenType) {
+        "kibana" { kubectl exec -it $podName -n $namespace -- bin/elasticsearch-service-tokens delete elastic/kibana kibana-token }
+        "fleet" { kubectl exec -it $podName -n $namespace -- bin/elasticsearch-service-tokens delete elastic/fleet-server fleet-server-token }
+        default { throw "Invalid token type specified. Use 'kibana' or 'fleet'." }
+    }
+}
+function Extract-Token {
+    param (
+        [string]$tokenOutput,
+        [string]$tokenType  # Accepts either "kibana" or "fleet"
+    )
+    switch ($tokenType) {
+        "kibana" { $pattern = "SERVICE_TOKEN elastic/kibana/kibana-token = (.+)" }
+        "fleet" { $pattern = "SERVICE_TOKEN elastic/fleet-server/fleet-server-token = (.+)" }
+        default { throw "Invalid token type specified. Use 'kibana' or 'fleet'." }
+    }
+    if ($tokenOutput -match $pattern) {
+        return $matches[1].Trim()
+    }
+    return $null
+}
+
+
 kubectl apply -f elasticsearch-configmap.yaml
 kubectl apply -f elasticsearch-secret.yaml
 kubectl apply -f elasticsearch-pvc.yaml
 kubectl apply -f elasticsearch-service.yaml
 kubectl apply -f elasticsearch-deployment.yaml
 
-# Wait for the Elasticsearch pod to be in the running state
 Write-Output "Waiting for Elasticsearch pod to be ready..."
 while ($true) {
     $podStatus = kubectl get pods -n $namespace -l $labelSelector -o jsonpath="{.items[0].status.conditions[?(@.type=='Ready')].status}"
         if ($podStatus -eq "True") {
         break
     }
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 1
 }
 $podName = kubectl get pods -n $namespace -l $labelSelector -o jsonpath="{.items[0].metadata.name}"
 Write-Output "Elasticsearch pod is ready: $podName"
 
-function Create-Token {
-    $tokenOutput = kubectl exec -it $podName -n $namespace -- bin/elasticsearch-service-tokens create elastic/kibana kibana-token 2>&1
-    return $tokenOutput
-}
-function Delete-Token {
-    kubectl exec -it $podName -n $namespace -- bin/elasticsearch-service-tokens delete elastic/kibana kibana-token
-}
-function Extract-Token {
-    param (
-        [string]$tokenOutput
-    )
-    if ($tokenOutput -match "SERVICE_TOKEN elastic/kibana/kibana-token = (.+)") {
-        return $matches[1].Trim()
-    }
-    return $null
-}
+$tokenOutput = Create-Token -tokenType "kibana"
 
-$tokenOutput = Create-Token
-
-if ($tokenOutput -match "ERROR: Service token \[elastic/kibana/kibana-token\] already exists") {
+if ($tokenOutput -like "*already exists*") {
     Write-Host "Token already exists. Deleting the old token."
-    Delete-Token
-    $tokenOutput = Create-Token
+    Delete-Token  -tokenType "kibana"
+    $tokenOutput = Create-Token  -tokenType "kibana"
 }
-
+Write-Host $tokenOutput
 $token = $tokenOutput.Trim()
-$token = Extract-Token -tokenOutput $tokenOutput
+$token = Extract-Token -tokenOutput $tokenOutput  -tokenType "kibana"
 
 if ($null -eq $token) {
     Write-Error "Failed to extract the token from the output."
     exit 1
 }
 
-Write-Host "Token obtained: $token"
+Write-Host "Kibana Token obtained: $token"
 
 $KibanaconfigMapYaml = Get-Content $KibanaconfigMapFile -Raw
 $FleetconfigMapYaml = Get-Content $FleetconfigMapFile -Raw
@@ -66,10 +87,32 @@ Set-Content $FleetconfigMapFile -Value $FleetconfigMapYaml
 Set-Content $KibanaconfigMapFile -Value $KibanaconfigMapYaml
 
 
-
 kubectl apply -f kibana-config.yaml
 kubectl apply -f kibana-deployment.yaml
 kubectl apply -f kibana-service.yaml
+
+$tokenOutput = Create-Token -tokenType "fleet"
+Write-Host $tokenOutput
+if ($tokenOutput -like "*already exists*") {
+    Write-Host "Token already exists. Deleting the old token."
+    Delete-Token  -tokenType "fleet"
+    $tokenOutput = Create-Token  -tokenType "fleet"
+}
+
+$token = $tokenOutput.Trim()
+$token = Extract-Token -tokenOutput $tokenOutput  -tokenType "fleet"
+
+if ($null -eq $token) {
+    Write-Error "Failed to extract the token from the output."
+    exit 1
+}
+
+Write-Host "Fleet Token obtained: $token"
+
+
+$FleetconfigMapYaml = $FleetconfigMapYaml -replace 'FLEET_SERVER_SERVICE_TOKEN: ".*"', "FLEET_SERVER_SERVICE_TOKEN: `"$token`""
+
+Set-Content $FleetconfigMapFile -Value $FleetconfigMapYaml
 
 
 kubectl apply -f fleet-server-config.yaml
