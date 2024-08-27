790dafc48ee768fb5481b747326df72f3dd9200e
Ilyes BEN MOUSSA <benmoussailyes13@gmail.com>
2024-08-27 23:29:22 +0200

    stable project working autoamtically just by running deploy script

--- undeploy-elk.ps1
+++ undeploy-elk.ps1
@@ -1,16 +1,9 @@
-# kubectl delete -f elasticsearch-configmap.yaml
-# kubectl delete -f elasticsearch-secret.yaml
-# kubectl delete -f elasticsearch-service.yaml
-# kubectl delete -f elasticsearch-deployment.yaml
 
-# kubectl delete -f logstash-configmap.yaml
-# kubectl delete -f logstash-service.yaml
-# kubectl delete -f logstash-deployment.yaml
-
-
-kubectl delete -f kibana-config.yaml
-kubectl delete -f kibana-deployment.yaml
-kubectl delete -f kibana-service.yaml
-
-kubectl delete -f fleet-server-deployment.yaml
-kubectl delete -f fleet-server-service.yaml
+kubectl delete all --all -n elk --force
+kubectl delete configmaps --all -n elk --force
+kubectl delete secrets --all -n elk --force
+kubectl delete pvc --all -n elk --force
+kubectl delete pv --all -n elk --force
+kubectl delete ingress --all -n elk --force
+kubectl delete pods --all -n elk --force
+kubectl delete deployment --all -n elk --force
